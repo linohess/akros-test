dbd41221e63da729bc5945700d4d113a9b3de423
linohess <lino.hess@bluewin.ch>
2023-08-25 14:16:26 +0200

    fixed shopping-list

--- src/Shopping-list.js
+++ src/Shopping-list.js
@@ -1,44 +1,15 @@
 import React, { useState } from 'react';
 
 function ShoppingForm() {
-  const [items, setItems] = useState([]);
-  const [position, setPosition] = useState('');
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [found, setFound] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newItem = { position, name, quantity, found };
-    setItems([...items, newItem]);
-    setPosition('');
-    setName('');
-    setQuantity('');
-    setFound(false);
+   
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Position:
-          <input type="text" value={position} onChange={(e) => setPosition(e.target.value)} />
-        </label>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          Quantity:
-          <input type="text" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-        </label>
-        <label>
-          Found:
-          <input type="checkbox" checked={found} onChange={() => setFound(!found)} />
-        </label>
-        <button type="submit">Add Item</button>
-      </form>
-      
+  
       <table>
         <thead>
           <tr>
@@ -49,16 +20,46 @@ function ShoppingForm() {
           </tr>
         </thead>
         <tbody>
-          {items.map((item, index) => (
-            <tr key={index}>
-              <td>{item.position}</td>
-              <td>{item.name}</td>
-              <td>{item.quantity}</td>
-              <td>{item.found ? '✔' : '❌'}</td>
-            </tr>
-          ))}
+        <tr>
+          <td>"1"</td>
+          <td>"Apples"</td>
+          <td>"5"</td>
+          <td> 
+            <div id="test-checkbox-1">
+              <input type="checkbox" />
+            </div></td>
+          </tr>
+          <tr>
+          <td>"2"</td>
+          <td>"Bananas"</td>
+          <td>"2"</td>
+          <td> 
+            <div id="test-checkbox-2">
+              <input type="checkbox" />
+            </div></td>
+          </tr>
+          <tr>
+          <td>"3"</td>
+          <td>"Bananas"</td>
+          <td>"2"</td>
+          <td> 
+            <div id="test-checkbox-2">
+              <input type="checkbox" />
+            </div></td>
+          </tr>
         </tbody>
       </table>
+      <form>
+      <label>
+        Trash:
+        <input type="text" name="trash" id="test-trash-input" />
+      </label>
+      <label>
+        Poop:
+        <input type="text" name="poop" id="test-poop-input" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
     </div>
   );
 }

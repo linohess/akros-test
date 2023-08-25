aa34cfb47062cfb34c28ca9fddd169eb3a00879e
linohess <lino.hess@bluewin.ch>
2023-08-25 13:43:15 +0200

    initial commit

---
+++ Test/test.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import jakarta.annotations.*;
+ 
+public class PageTest {
+ 
+        public static void main(String[] args) throws IOException, InterruptedException {
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+ 
+                WebDriver driver = new ChromeDriver(chromeOptions);
+ 
+                driver.get("localhost:3000");
+ 
+                Thread.sleep(1000);
+ 
+               
+                driver.quit();
+        }
+
+        @Test
+        public void clickBaitTest(){
+
+        }
+}
